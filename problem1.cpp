4e59dee1e8a00d6b5b7dc34cdd16f2a5b4e5957a
Sakalya Mitra <70064084+Sakalya100@users.noreply.github.com>
2022-10-22 12:01:33 +0530

    Update problem1.cpp

--- problem1.cpp
+++ problem1.cpp
@@ -4,10 +4,7 @@ using namespace std;
 int main() {
     // Write C++ code here
     int x1,x2,y1,y2;
-    cin>>x1;
-    cin>>x2;
-    cin>>y1;
-    cin>>y2;
+    cin>> x1 >> x2 >> y1 >>y2;
     
     float result = ((float)(y2-y1))/(x2-x1);
     cout<<result;
